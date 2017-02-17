bf4f80eb62029fd0535f7c5d279b22ef8775c415
SebastienForay <sebastien.foray@ynov.com>
2017-02-17 12:35:51 +0100

    Auto create database and insert admin user

---
+++ WebContent/WEB-INF/classes/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (`id`, `email`, `firstname`, `lastname`, `password`, `type`) VALUES (1, 'admin@creditauven.ce', 'Administateur', 'Administateur', 'a12892155820f9b5c0ffa697e56968808ffa7cd4', 2);
